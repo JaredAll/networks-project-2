de3369e53e8c0a3371043d0fc86464a852145f61
Jon Marple <elpramnoj@gmail.com>
2019-04-10 17:36:16 -0500

    Set up sample code to retrieve http response codes, modified header fields, and html body response

--- src/networks/WebTest.java
+++ src/networks/WebTest.java
@@ -1,69 +1,47 @@
 package networks;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Date;
 
 public class WebTest {
 
   public static void main(String[] args) throws IOException {
-    String targetURL = "http://www.google.com/index.htm";
-    //System.out.println("Response: " + executePost(targetURL, ""));
-    //System.out.println("Response: " + sendPost(targetURL));
-    System.out.println("Response: " + TCPPost(targetURL));
-  }
-  
-  public static String executePost(String targetURL, String urlParameters) {
-    HttpURLConnection connection = null;
-    try {
-      // Create connection
-      URL url = new URL(targetURL);
-      connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("POST");
-      connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-      connection.setRequestProperty("Content-Length", Integer.toString(urlParameters.getBytes().length));
-      connection.setRequestProperty("Content-Language", "en-US");  
-      connection.setUseCaches(false);
-      connection.setDoOutput(true);
-
-      // Send request
-      DataOutputStream wr = new DataOutputStream (connection.getOutputStream());
-      wr.writeBytes(urlParameters);
-      wr.close();
-
-      // Get Response  
-      InputStream is = connection.getInputStream();
-      BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-      StringBuilder response = new StringBuilder();
-      String line;
-      while ((line = rd.readLine()) != null) {
-        response.append(line);
-        response.append('\r');
-      }
-      rd.close();
-      return response.toString();
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    } finally {
-      if (connection != null) {
-        connection.disconnect();
-      }
-    }
-  }
-  
-  public static String sendPost(String targetURL) throws IOException {
+    
+    // Creating URL object of website to talk to
+    String targetURL = "https://www.wikipedia.org/";
     URL url = new URL(targetURL);
-    URLConnection urlCon = url.openConnection();
+    
+    // Creating URL connection and http URL connection
+    URLConnection urlConnection = url.openConnection();
+    HttpURLConnection httpConnection = (HttpURLConnection)urlConnection;
+    
+    // Setting If-Modified-Since header field to 1 hour ago
+    httpConnection.setIfModifiedSince(System.currentTimeMillis()-1000*60*60);
+    
+    // Setting request method to GET and opening http URL connection
+    httpConnection.setRequestMethod("GET");
+    httpConnection.connect();
+    
+    // Printing out Status Code
+    System.out.println("Code: " + httpConnection.getResponseCode());
+    
+    // Printing out If-Modified-Since header field
+    System.out.println("If-Modified-Since: " + new Date(httpConnection.getIfModifiedSince()).toString());
+    
+    // Printing out Last-Modified header field
+    System.out.println("Last-Modified: " + new Date(httpConnection.getLastModified()).toString());
+    
+    // Printing out http request response
+    System.out.println("Response: " + sendRequest(urlConnection));
+    
+  }
+  
+  public static String sendRequest(URLConnection urlCon) throws IOException {
     BufferedReader in = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
     String inputLine;
     String response = "";
@@ -72,35 +50,5 @@ public class WebTest {
     in.close();
     return response;
   }
-  
-  public static String TCPPost(String targetURL) throws IOException {
-    String pageAddr = targetURL;
-    URL url = new URL(pageAddr);
-    String websiteAddress = url.getHost();
-
-    String file = url.getFile();
-    Socket clientSocket = new Socket(websiteAddress, 80);
-
-    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-    OutputStreamWriter outWriter = new OutputStreamWriter(clientSocket.getOutputStream());
-    outWriter.write("GET " + file + " HTTP/1.0\r\n\n");
-    outWriter.flush();
-    BufferedWriter out = new BufferedWriter(new FileWriter(file));
-    boolean more = true;
-    String input;
-    while (more) {
-      input = inFromServer.readLine();
-      if (input == null)
-        more = false;
-      else {
-        out.write(input);
-      }
-    }
-    out.close();
-    clientSocket.close();
-    return "";
-  }
-
 
 }
