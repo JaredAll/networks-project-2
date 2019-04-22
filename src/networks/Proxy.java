20815ab6a47f3e7444ac5608533c086d4e361718
Jonathan Marple <elpramnoj@gmail.com>
2019-04-22 13:06:51 -0500

    Finished hooking up wget to project, both i/o

--- src/networks/Proxy.java
+++ src/networks/Proxy.java
@@ -12,45 +12,68 @@ public class Proxy {
   public static void main(String[] args) {
     
     Cache cache = new Cache();
-
-    // Sample code to make a web request
-    WebRequest request = null;
-    try {
-      request = new WebRequest("https://google.com/"); // Initializing web request
-      request.setIfModifiedSince(System.currentTimeMillis()); // Setting if-modified-since header (pulled from timestamp hashmap)
-      request.connect(); // Start the connection
-      request.sendRequest(); // Sending http request
-      // If request status code = 304, no html response update is required, but we should update the timestamp for the request
-      System.out.println("Status: " + request.getStatusCode() + "\nLast Modified: " + request.getLastModified() + "\nIf Modified Since: " + request.getIfModifiedSince());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
     
     ServerSocket serverSocket = null;
     Socket socket = null;
     InputStream inStream = null;
     OutputStream outStream = null;
+    
     try {
+      
+      // Socket used to listen for new clients
       serverSocket = new ServerSocket(6969);
+      
       // Main loop that receives incoming requests and handles them accordingly
       while (true) {
+        
+        System.out.println("\nWaiting for a client...");
+        
+        // Setting up socket and streams, waits on .accept() until a new client appears
         socket = serverSocket.accept();
         inStream = socket.getInputStream();        
         outStream = socket.getOutputStream();
+        
+        // Printing proxy server port and client port
         System.out.println("\nConnected");
         System.out.println("Client Port: " + socket.getPort());
         System.out.println("Server Port: " + socket.getLocalPort());
+        
+        // Receiving and printing wget's HTTP request
         int available = inStream.available();
         byte[] b = new byte[available];
         inStream.read(b, 0, available);
-        System.out.println("\n" + new String(b));
-        outStream.write(request.getResponse().getBytes());
-        System.out.println("\n" + request.getResponse());
-      }     
+        String request = new String(b);
+        System.out.println("\n" + request);
+        
+        // Retreiving, forwarding, and printing response
+        String response = sendWebRequest(request.split(" ")[1], 0);
+        outStream.write(response.getBytes());
+        System.out.println("\n" + response);
+        socket.close();
+        
+      }
     } catch (IOException io) {
       io.printStackTrace();
     }
     
+  }
+  
+  private static String sendWebRequest(String targetURL, int ifModifiedSince) {
+    WebRequest request = null;
+    try {
+      request = new WebRequest(targetURL); // Initializing web request
+      if (ifModifiedSince != 0) {        
+        request.setIfModifiedSince(System.currentTimeMillis() - ifModifiedSince); // Setting if-modified-since header (pulled from timestamp hashmap)
+      }
+      request.connect(); // Start the connection
+      request.sendRequest(); // Sending http request
+      // If request status code = 304, no html response update is required, but we should update the timestamp for the request
+      System.out.println("Status: " + request.getStatusCode() + "\nLast Modified: " + request.getLastModified() + "\nIf Modified Since: " + request.getIfModifiedSince());
+      return request.getResponse();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
   }
   
 }
